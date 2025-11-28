0fa641f655e16acdc0f5635d6d6fca3fe0ee661b
Kidane Negash <kidanenegash1994@gmail.com>
2025-11-28 15:41:53 +0300

    Updating the port number onthe frontend side.

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "http://localhost:10000/api/v1/user/logout",
         {
           withCredentials: true,
         }
